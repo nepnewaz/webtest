4276200ac7f76247f458720d8c8dc8725d3f5f48
nepnewaz <82073683+nepnewaz@users.noreply.github.com>
2024-12-24 11:11:08 +0545

    web update

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-
+const navLinks = document.getElementById("nav-links");
+const menuBtn = document.getElementById("menu-btn");
 const scrollRevealOption = {
     distance: "50px",
     origin: "bottom",
     duration: 1000,
   };
   
+  menuBtn.addEventListener("click", (e) => {
+    navLinks.classList.toggle("open");
   
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+  });
+  
+  navLinks.addEventListener("click", (e) => {
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-line");
+  });
   
   ScrollReveal().reveal(".register__image img", {
     ...scrollRevealOption,
@@ -20,4 +31,11 @@ const scrollRevealOption = {
     delay: 1000,
   });
   
+  const instagram = document.querySelector(".instagram__flex");
+
+Array.from(instagram.children).forEach((item) => {
+  const duplicateNode = item.cloneNode(true);
+  duplicateNode.setAttribute("aria-hidden", true);
+  instagram.appendChild(duplicateNode);
+});
   
